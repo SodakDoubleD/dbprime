377244240cee7ec014b656129318d0e139532d61
SodakDoubleD <sodakdoubled@gmail.com>
2019-08-06 20:17:18 -0500

    fleshing out documentation

--- dbprime.py
+++ dbprime.py
@@ -2,7 +2,15 @@
 
 # ===============================================================
 class MockRecord(object):
-	def __init__(self, db_connection):
+	'''
+	Requires a database connection object that conforms to the
+	PEP-249 database API specification.
+
+	Unpacks and inserts a database record based on the passed in kwargs.
+	The initialized object contains the newly inserted primary key value.
+	'''
+	# -----------------------------------------------------------
+	def __init__(self, db_connection, primary_key_column, **kwargs):
 		self.db_connection = db_connection
 		pass
 
