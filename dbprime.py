223c93c0843311028fac00aee9924bca610ae036
SodakDoubleD <sodakdoubled@gmail.com>
2019-08-07 20:45:17 -0500

    pylint compatibility pls

--- dbprime.py
+++ dbprime.py
@@ -1,17 +1,20 @@
 
 
-class MockRecord(object):
-	'''
-	Requires a database connection object that conforms to the
-	PEP-249 database API specification.
+class MockRecord:
+    '''
+    Requires a database connection object that conforms to the
+    PEP-249 database API specification.
 
-	Unpacks and inserts a database record based on the passed in kwargs.
-	The initialized object contains the newly inserted primary key value.
-	'''
+    Unpacks and inserts a database record based on the passed in kwargs.
+    The initialized object contains the newly inserted primary key value.
+    '''
 
-	def __init__(self, db_connection, primary_key_column, **kwargs):
-		self.db_connection = db_connection
-		pass
+    def __init__(self, db_connection, primary_key_column, **kwargs):
+        self.db_connection = db_connection
+        self.primary_key_column = primary_key_column
 
-	def __del__(self):
-		pass
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __del__(self):
+        pass
