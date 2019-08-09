8f957446d41b60549f22795972294e5e1cef4d3b
SodakDoubleD <sodakdoubled@gmail.com>
2019-08-08 22:09:41 -0500

    add unit test file and __init__

---
+++ test_dbprime.py
@@ -0,0 +1,37 @@
+# import dbprime
+
+# import psycopg2
+# import MySQLdb
+import unittest
+
+
+class TestPostgres(unittest.TestCase):
+
+    def test_postgres_insert(self):
+        pass
+
+    def test_postgres_update(self):
+        pass
+
+    def test_postgres_delete(self):
+        pass
+
+
+class TestMySQL(unittest.TestCase):
+
+    def test_mysql_insert(self):
+        pass
+
+    def test_mysql_update(self):
+        pass
+
+    def test_mysql_delete(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
