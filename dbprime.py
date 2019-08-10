13a5a06c4732fafa1cc06bb7a1bae0dafc3e3521
SodakDoubleD <sodakdoubled@gmail.com>
2019-08-09 22:21:23 -0500

    not tested, but started filling out functions

--- dbprime.py
+++ dbprime.py
@@ -1,4 +1,4 @@
-# import os
+import psycopg2
 
 class MockRecord:
     '''
@@ -9,18 +9,29 @@ class MockRecord:
     The initialized object contains the newly inserted primary key value.
     '''
 
-    def __init__(self, database_module, database_args, primary_key_column, **kwargs):
+    def __init__(self, database_module, database_args, table_name, primary_key_column, **kwargs):
         # Check if we can even handle the db module before trying anything else
         try:
-            self.insertion_handler = self.__class__.insertion_handlers[database_module.__name__]
+            self.insertion_handler = \
+                self.__class__.insertion_handlers[database_module.__name__]
+
         except KeyError:
             error_message = 'Unhandled database module: {} Supported modules: {}'\
-                .format(database_module.__name__, self.__class__.insertion_handlers.keys())
+                .format(database_module.__name__,
+                        self.__class__.insertion_handlers.keys())
+
             raise KeyError(error_message)
 
-        self._db_connection = database_module.connect(**database_args)
-        self._db_cursor = self._db_connection.cursor()
-        self._pk_column = primary_key_column
+        try:
+            self._db_connection = database_module.connect(**database_args)
+            self._db_cursor = self._db_connection.cursor()
+        except Exception as e:
+            print(e)
+            raise
+
+        self.table_name = table_name
+        self.pk_column = primary_key_column
+        self.columns = sorted([kwargs.keys()])
 
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -28,12 +39,27 @@ class MockRecord:
     def __del__(self):
         # delete inserted record
         # close db connection
-        pass
+        sql = 'DELETE FROM {} WHERE {} = {};'.format(self.table_name,
+                                                     self.pk_column,
+                                                     getattr(self, self.pk_column))
+        self._db_cursor.execute(sql)
+        self._db_connection.close()
 
     def _insert_postgres_record(self):
         # insert a record into the db using the db connection
         # set the value of the primary key as an attr on the object instance
-        pass
+        sql = """
+            INSERT INTO {} ({})
+            VALUES ({})
+            RETURNING {};
+        """.format(self.table_name,
+                   ', '.join(self.columns),
+                   ', '.join([getattr(self, key) for key in self.columns]))
+
+        self._db_cursor.execute(sql)
+
+        primary_key = self._db_cursor.fetchone()[0]
+        setattr(self, self.pk_column, primary_key)
 
     def _insert_mysql_record(self):
         # insert a record into the db using the db connection
@@ -45,8 +71,14 @@ class MockRecord:
         'MySQLdb': _insert_mysql_record,
     }
 
-# def test():
-#     test = MockRecord(os, {}, 'test')
+def test():
+    dbargs = {
+        'user': 'test',
+        'password': 'test',
+        'db': '',
+        'host': ''
+    }
+    mock_rec = MockRecord(psycopg2, dbargs, 'test_table', 'test_tableid')
 
-# if __name__ == '__main__':
-#     test()
+if __name__ == '__main__':
+    test()
