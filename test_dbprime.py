a1839661e44d899ba82c35ba5568a2c334d82bdc
SodakDoubleD <sodakdoubled@gmail.com>
2019-08-11 00:53:33 -0500

    add postgres-specific unit tests

--- test_dbprime.py
+++ test_dbprime.py
@@ -1,20 +1,68 @@
-import dbprime
-
+import unittest
 import psycopg2
 # import MySQLdb
-import unittest
+
+from dbprime import MockRecord
 
 
 class TestPostgres(unittest.TestCase):
 
+    def setUp(self):
+        self.dbargs = {
+            'user': 'ddirk',
+            'password': 'test',
+            'database': 'test_db',
+            'host': 'localhost'
+        }
+        self.dbcon = psycopg2.connect(**self.dbargs)
+        self.dbcur = self.dbcon.cursor()
+
+    def tearDown(self):
+        self.dbcon.close()
+
     def test_postgres_insert(self):
-        pass
+        test_obj = MockRecord(psycopg2, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+        self.dbcur.execute("""
+            SELECT EXISTS (
+                SELECT 1
+                FROM test_table
+                WHERE test_tableid = {}
+            );
+            """.format(test_obj.test_tableid))
+
+        record_exists = self.dbcur.fetchone()[0]
+        self.assertTrue(record_exists)
 
     def test_postgres_update(self):
-        pass
+        test_obj = MockRecord(psycopg2, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+
+        self.dbcur.execute("""
+            UPDATE test_table SET num_val = 456 WHERE test_tableid = {};
+            """.format(test_obj.test_tableid))
+
+        self.dbcur.execute("""
+            SELECT num_val = 456 FROM test_table WHERE test_tableid = {};
+            """.format(test_obj.test_tableid))
+
+        self.dbcon.commit()
+        update_succeeded = self.dbcur.fetchone()[0]
+        self.assertTrue(update_succeeded)
 
     def test_postgres_delete(self):
-        pass
+        test_obj = MockRecord(psycopg2, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+        test_tableid = test_obj.test_tableid
+        del test_obj
+
+        self.dbcur.execute("""
+            SELECT EXISTS (
+                SELECT 1
+                FROM test_table
+                WHERE test_tableid = {}
+            );
+            """.format(test_tableid))
+
+        record_exists = self.dbcur.fetchone()[0]
+        self.assertFalse(record_exists)
 
 
 class TestMySQL(unittest.TestCase):
@@ -30,8 +78,7 @@ class TestMySQL(unittest.TestCase):
 
 
 def main():
-    pass
-
+    unittest.main()
 
 if __name__ == '__main__':
     main()
