89b941bbe16a40b51c311c2c5734cceb72471a38
SodakDoubleD <sodakdoubled@gmail.com>
2019-08-19 19:45:02 -0500

    take a crack at mysql testing

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -4,64 +4,64 @@ import MySQLdb
 from dbprime import MockRecord
 
 
-# class TestMySQL(unittest.TestCase):
+class TestMySQL(unittest.TestCase):
 
-#     def setUp(self):
-#         self.dbargs = {
-#             'user': 'root',
-#             'password': '',
-#             'database': 'mysql',
-#             'host': '127.0.0.1'
-#         }
-#         self.dbcon = MySQLdb.connect(**self.dbargs)
-#         self.dbcur = self.dbcon.cursor()
+    def setUp(self):
+        self.dbargs = {
+            'user': 'root',
+            'password': '',
+            'database': 'mysql',
+            'host': '127.0.0.1'
+        }
+        self.dbcon = MySQLdb.connect(**self.dbargs)
+        self.dbcur = self.dbcon.cursor()
 
-#     def tearDown(self):
-#         self.dbcon.close()
+    def tearDown(self):
+        self.dbcon.close()
 
-#     def test_mysql_insert(self):
-#         test_obj = MockRecord(MySQLdb, self.dbargs, 'test_table', 'test_tableid', num_val=123)
-#         self.dbcur.execute("""
-#             SELECT EXISTS (
-#                 SELECT 1
-#                 FROM test_table
-#                 WHERE test_tableid = {}
-#             );
-#             """.format(test_obj.test_tableid))
+    def test_mysql_insert(self):
+        test_obj = MockRecord(MySQLdb, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+        self.dbcur.execute("""
+            SELECT EXISTS (
+                SELECT 1
+                FROM test_table
+                WHERE test_tableid = {}
+            );
+            """.format(test_obj.test_tableid))
 
-#         record_exists = self.dbcur.fetchone()[0]
-#         self.assertTrue(record_exists)
+        record_exists = self.dbcur.fetchone()[0]
+        self.assertTrue(record_exists)
 
-#     def test_mysql_update(self):
-#         test_obj = MockRecord(MySQLdb, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+    def test_mysql_update(self):
+        test_obj = MockRecord(MySQLdb, self.dbargs, 'test_table', 'test_tableid', num_val=123)
 
-#         self.dbcur.execute("""
-#             UPDATE test_table SET num_val = 456 WHERE test_tableid = {};
-#             """.format(test_obj.test_tableid))
+        self.dbcur.execute("""
+            UPDATE test_table SET num_val = 456 WHERE test_tableid = {};
+            """.format(test_obj.test_tableid))
 
-#         self.dbcur.execute("""
-#             SELECT num_val = 456 FROM test_table WHERE test_tableid = {};
-#             """.format(test_obj.test_tableid))
+        self.dbcur.execute("""
+            SELECT num_val = 456 FROM test_table WHERE test_tableid = {};
+            """.format(test_obj.test_tableid))
 
-#         self.dbcon.commit()
-#         update_succeeded = self.dbcur.fetchone()[0]
-#         self.assertTrue(update_succeeded)
+        self.dbcon.commit()
+        update_succeeded = self.dbcur.fetchone()[0]
+        self.assertTrue(update_succeeded)
 
-#     def test_mysql_delete(self):
-#         test_obj = MockRecord(MySQLdb, self.dbargs, 'test_table', 'test_tableid', num_val=123)
-#         test_tableid = test_obj.test_tableid
-#         del test_obj
+    def test_mysql_delete(self):
+        test_obj = MockRecord(MySQLdb, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+        test_tableid = test_obj.test_tableid
+        del test_obj
 
-#         self.dbcur.execute("""
-#             SELECT EXISTS (
-#                 SELECT 1
-#                 FROM test_table
-#                 WHERE test_tableid = {}
-#             );
-#             """.format(test_tableid))
+        self.dbcur.execute("""
+            SELECT EXISTS (
+                SELECT 1
+                FROM test_table
+                WHERE test_tableid = {}
+            );
+            """.format(test_tableid))
 
-#         record_exists = self.dbcur.fetchone()[0]
-#         self.assertFalse(record_exists)
+        record_exists = self.dbcur.fetchone()[0]
+        self.assertFalse(record_exists)
 
 
 def main():
