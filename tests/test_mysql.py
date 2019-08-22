6f5c4de4ba12c4d5f1a289f19c098c3b34355a09
SodakDoubleD <sodakdoubled@gmail.com>
2019-08-21 21:15:56 -0500

    format issues in mysql tests

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -20,7 +20,9 @@ class TestMySQL(unittest.TestCase):
         self.dbcon.close()
 
     def test_mysql_insert(self):
-        test_obj = MockMySQLRecord(MySQLdb, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+        test_obj = MockMySQLRecord(MySQLdb, self.dbargs,
+                                   'test_table', 'test_tableid',
+                                   num_val=123)
         self.dbcur.execute("""
             SELECT EXISTS (
                 SELECT 1
@@ -33,7 +35,9 @@ class TestMySQL(unittest.TestCase):
         self.assertTrue(record_exists)
 
     def test_mysql_update(self):
-        test_obj = MockMySQLRecord(MySQLdb, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+        test_obj = MockMySQLRecord(MySQLdb, self.dbargs,
+                                   'test_table', 'test_tableid',
+                                   num_val=123)
 
         self.dbcur.execute("""
             UPDATE test_table SET num_val = 456 WHERE test_tableid = {};
@@ -48,7 +52,9 @@ class TestMySQL(unittest.TestCase):
         self.assertTrue(update_succeeded)
 
     def test_mysql_delete(self):
-        test_obj = MockMySQLRecord(MySQLdb, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+        test_obj = MockMySQLRecord(MySQLdb, self.dbargs,
+                                   'test_table', 'test_tableid',
+                                   num_val=123)
         test_tableid = test_obj.test_tableid
         del test_obj
 
