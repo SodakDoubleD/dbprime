3b4161acb04c00c4a3add510288f90f5b24335f0
SodakDoubleD <sodakdoubled@gmail.com>
2019-08-21 21:14:39 -0500

    format issues in postgres tests

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -20,7 +20,9 @@ class TestPostgres(unittest.TestCase):
         self.dbcon.close()
 
     def test_postgres_insert(self):
-        test_obj = MockPostgresRecord(psycopg2, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+        test_obj = MockPostgresRecord(psycopg2, self.dbargs,
+                                      'test_table', 'test_tableid',
+                                      num_val=123)
         self.dbcur.execute("""
             SELECT EXISTS (
                 SELECT 1
@@ -33,7 +35,9 @@ class TestPostgres(unittest.TestCase):
         self.assertTrue(record_exists)
 
     def test_postgres_update(self):
-        test_obj = MockPostgresRecord(psycopg2, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+        test_obj = MockPostgresRecord(psycopg2, self.dbargs,
+                                      'test_table', 'test_tableid',
+                                      num_val=123)
 
         self.dbcur.execute("""
             UPDATE test_table SET num_val = 456 WHERE test_tableid = {};
@@ -48,7 +52,9 @@ class TestPostgres(unittest.TestCase):
         self.assertTrue(update_succeeded)
 
     def test_postgres_delete(self):
-        test_obj = MockPostgresRecord(psycopg2, self.dbargs, 'test_table', 'test_tableid', num_val=123)
+        test_obj = MockPostgresRecord(psycopg2, self.dbargs,
+                                      'test_table', 'test_tableid',
+                                      num_val=123)
         test_tableid = test_obj.test_tableid
         del test_obj
 
