b9be56009d556c8b34547859f0186fe761aedee9
SodakDoubleD <sodakdoubled@gmail.com>
2019-09-09 21:09:12 -0500

    setup.py additions

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup
 
+with open('README.md', 'r') as fh:
+    long_descrition = fh.read()
+
 setup(
     name='dbprime',
     version='0.1dev',
+    author='Dalton Dirkson',
+    author_email='sodakdoubled@gmail.com',
     packages=['dbprime',],
 )
