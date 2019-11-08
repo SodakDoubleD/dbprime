76d2824adbe0f10d6ad04a5607a07f36874389c7
SodakDoubleD <sodakdoubled@gmail.com>
2019-11-07 20:23:25 -0600

    run black code formatter against tests directory

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -5,13 +5,12 @@ from dbprime import MockMySQLRecord
 
 
 class TestMySQL(unittest.TestCase):
-
     def setUp(self):
         self.dbargs = {
-            'user': 'root',
-            'password': '',
-            'database': 'test_db',
-            'host': '127.0.0.1'
+            "user": "root",
+            "password": "",
+            "database": "test_db",
+            "host": "127.0.0.1",
         }
         self.dbcon = MySQLdb.connect(**self.dbargs)
         self.dbcur = self.dbcon.cursor()
@@ -20,54 +19,79 @@ class TestMySQL(unittest.TestCase):
         self.dbcon.close()
 
     def test_mysql_insert(self):
-        test_obj = MockMySQLRecord(MySQLdb, self.dbargs,
-                                   table_name='test_table',
-                                   primary_key_column='test_tableid',
-                                   num_val=123)
-        self.dbcur.execute("""
+        test_obj = MockMySQLRecord(
+            MySQLdb,
+            self.dbargs,
+            table_name="test_table",
+            primary_key_column="test_tableid",
+            num_val=123,
+        )
+        self.dbcur.execute(
+            """
             SELECT EXISTS (
                 SELECT 1
                 FROM test_table
                 WHERE test_tableid = {}
             );
-            """.format(test_obj.test_tableid))
+            """.format(
+                test_obj.test_tableid
+            )
+        )
 
         record_exists = self.dbcur.fetchone()[0]
         self.assertTrue(record_exists)
 
     def test_mysql_update(self):
-        test_obj = MockMySQLRecord(MySQLdb, self.dbargs,
-                                   table_name='test_table',
-                                   primary_key_column='test_tableid',
-                                   num_val=123)
+        test_obj = MockMySQLRecord(
+            MySQLdb,
+            self.dbargs,
+            table_name="test_table",
+            primary_key_column="test_tableid",
+            num_val=123,
+        )
 
-        self.dbcur.execute("""
+        self.dbcur.execute(
+            """
             UPDATE test_table SET num_val = 456 WHERE test_tableid = {};
-            """.format(test_obj.test_tableid))
+            """.format(
+                test_obj.test_tableid
+            )
+        )
 
-        self.dbcur.execute("""
+        self.dbcur.execute(
+            """
             SELECT num_val = 456 FROM test_table WHERE test_tableid = {};
-            """.format(test_obj.test_tableid))
+            """.format(
+                test_obj.test_tableid
+            )
+        )
 
         self.dbcon.commit()
         update_succeeded = self.dbcur.fetchone()[0]
         self.assertTrue(update_succeeded)
 
     def test_mysql_delete(self):
-        test_obj = MockMySQLRecord(MySQLdb, self.dbargs,
-                                   table_name='test_table',
-                                   primary_key_column='test_tableid',
-                                   num_val=123)
+        test_obj = MockMySQLRecord(
+            MySQLdb,
+            self.dbargs,
+            table_name="test_table",
+            primary_key_column="test_tableid",
+            num_val=123,
+        )
         test_tableid = test_obj.test_tableid
         del test_obj
 
-        self.dbcur.execute("""
+        self.dbcur.execute(
+            """
             SELECT EXISTS (
                 SELECT 1
                 FROM test_table
                 WHERE test_tableid = {}
             );
-            """.format(test_tableid))
+            """.format(
+                test_tableid
+            )
+        )
 
         record_exists = self.dbcur.fetchone()[0]
         self.assertFalse(record_exists)
@@ -76,5 +100,6 @@ class TestMySQL(unittest.TestCase):
 def main():
     unittest.main()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -5,13 +5,12 @@ from dbprime import MockPostgresRecord
 
 
 class TestPostgres(unittest.TestCase):
-
     def setUp(self):
         self.dbargs = {
-            'user': 'postgres',
-            'password': '',
-            'database': 'postgres',
-            'host': 'localhost'
+            "user": "postgres",
+            "password": "",
+            "database": "postgres",
+            "host": "localhost",
         }
         self.dbcon = psycopg2.connect(**self.dbargs)
         self.dbcur = self.dbcon.cursor()
@@ -20,54 +19,79 @@ class TestPostgres(unittest.TestCase):
         self.dbcon.close()
 
     def test_postgres_insert(self):
-        test_obj = MockPostgresRecord(psycopg2, self.dbargs,
-                                      table_name='test_table',
-                                      primary_key_column='test_tableid',
-                                      num_val=123)
-        self.dbcur.execute("""
+        test_obj = MockPostgresRecord(
+            psycopg2,
+            self.dbargs,
+            table_name="test_table",
+            primary_key_column="test_tableid",
+            num_val=123,
+        )
+        self.dbcur.execute(
+            """
             SELECT EXISTS (
                 SELECT 1
                 FROM test_table
                 WHERE test_tableid = {}
             );
-            """.format(test_obj.test_tableid))
+            """.format(
+                test_obj.test_tableid
+            )
+        )
 
         record_exists = self.dbcur.fetchone()[0]
         self.assertTrue(record_exists)
 
     def test_postgres_update(self):
-        test_obj = MockPostgresRecord(psycopg2, self.dbargs,
-                                      table_name='test_table',
-                                      primary_key_column='test_tableid',
-                                      num_val=123)
+        test_obj = MockPostgresRecord(
+            psycopg2,
+            self.dbargs,
+            table_name="test_table",
+            primary_key_column="test_tableid",
+            num_val=123,
+        )
 
-        self.dbcur.execute("""
+        self.dbcur.execute(
+            """
             UPDATE test_table SET num_val = 456 WHERE test_tableid = {};
-            """.format(test_obj.test_tableid))
+            """.format(
+                test_obj.test_tableid
+            )
+        )
 
-        self.dbcur.execute("""
+        self.dbcur.execute(
+            """
             SELECT num_val = 456 FROM test_table WHERE test_tableid = {};
-            """.format(test_obj.test_tableid))
+            """.format(
+                test_obj.test_tableid
+            )
+        )
 
         self.dbcon.commit()
         update_succeeded = self.dbcur.fetchone()[0]
         self.assertTrue(update_succeeded)
 
     def test_postgres_delete(self):
-        test_obj = MockPostgresRecord(psycopg2, self.dbargs,
-                                      table_name='test_table',
-                                      primary_key_column='test_tableid',
-                                      num_val=123)
+        test_obj = MockPostgresRecord(
+            psycopg2,
+            self.dbargs,
+            table_name="test_table",
+            primary_key_column="test_tableid",
+            num_val=123,
+        )
         test_tableid = test_obj.test_tableid
         del test_obj
 
-        self.dbcur.execute("""
+        self.dbcur.execute(
+            """
             SELECT EXISTS (
                 SELECT 1
                 FROM test_table
                 WHERE test_tableid = {}
             );
-            """.format(test_tableid))
+            """.format(
+                test_tableid
+            )
+        )
 
         record_exists = self.dbcur.fetchone()[0]
         self.assertFalse(record_exists)
@@ -76,5 +100,6 @@ class TestPostgres(unittest.TestCase):
 def main():
     unittest.main()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
