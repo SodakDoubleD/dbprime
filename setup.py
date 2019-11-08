2cff1741e8fe5eb808e542f1927041058900e3f2
SodakDoubleD <sodakdoubled@gmail.com>
2019-11-07 20:17:55 -0600

    run black code formatter against setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
-with open('README.md', 'r') as fh:
-    long_descrition = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='dbprime',
-    version='0.1dev',
-    author='Dalton Dirkson',
-    author_email='sodakdoubled@gmail.com',
-    packages=['dbprime',],
+    name="dbprime",
+    version="0.1dev",
+    author="Dalton Dirkson",
+    author_email="sodakdoubled@gmail.com",
+    packages=["dbprime",],
 )
